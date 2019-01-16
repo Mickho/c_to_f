2912ec720252963ea886a9912d2d784d88cf759b
Mick Ho <mick.my.ho@gmail.com>
2019-01-16 22:35:04 +0800

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,9 @@
+Celsius = input("Please input Celsius num :")
+x = float(Celsius)
+y = x * (9 / 5) + 32
+print('Fahrenheit is:', y)
+
+Fahrenheit = input("Please input Fahrenheit num :")
+x = float(Fahrenheit)
+y = (5 / 9) * (x - 32)
+print('Celsius is :', y)
